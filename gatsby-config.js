3a12d65c0bad78a1f20b776d5381cf66a9fb7ccf
Mads <mkrn@itu.dk>
2018-12-27 15:02:06 +0100

    remove unused plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,13 +19,6 @@ module.exports = {
         accessToken,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
