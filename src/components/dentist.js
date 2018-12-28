b32982a50c8c300ec905f90d69d69cd0a3319ae6
Mads <mkrn@itu.dk>
2018-12-28 17:15:03 +0100

    Make employee image closer to text on large screen

--- src/components/dentist.js
+++ src/components/dentist.js
@@ -3,8 +3,8 @@ import SampleImage from '../assets/Sample_Profile.jpg'
 
 const Dentist = ({ info }) => {
   return (
-    <div className="mb-12 flex flex-col sm:flex-row md:w-4/5 mx-auto">
-      <div className="mb-4 mx-auto w-4/5 sm:w-1/3 sm:mr-4 lg:mr-0">
+    <div className="mb-12 flex flex-col sm:flex-row mx-auto">
+      <div className="mb-4 mx-auto sm:mr-4 lg:mr-0">
         <img
           className="block rounded-full"
           src={info.billede ? info.billede.file.url : SampleImage}
@@ -12,7 +12,9 @@ const Dentist = ({ info }) => {
         />
       </div>
       <div className="mx-auto w-4/5 sm:w-2/3">
-        <h4 className="text-blue-dark mb-1">{info.titelOgNavn}</h4>
+        <h4 className="text-blue-dark mb-1 text-center sm:text-left">
+          {info.titelOgNavn}
+        </h4>
         <p className="text-body">{info.beskrivelse.beskrivelse}</p>
       </div>
     </div>
