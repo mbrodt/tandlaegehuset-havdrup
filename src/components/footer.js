9e214a950b334ca78a931fbedf9933b17844104c
Mads <mkrn@itu.dk>
2019-06-30 20:54:00 +0200

    remove w-screen footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="bg-brown-lightest w-screen text-sm p-4 text-center flex flex-col text-footer font-semibold mt-24">
+    <div className="bg-brown-lightest text-sm p-4 text-center flex flex-col text-footer font-semibold mt-24">
       <p>Lene Marquardsen | Sabine Lauge Olsen</p>
       <p>
         Hovedgaden 28 | 4622 Havdrup Tlf. 46 18 55 80
