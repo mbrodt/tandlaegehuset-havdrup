bcf3a71cdb6d75ddfc7a226d2483d904fc43510b
Mads Brodt <madsbrodt3@gmail.com>
2023-06-09 20:29:19 +0200

    Remove Lene and add Gry

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="bg-brown-lightest text-sm p-4 text-center flex flex-col text-footer font-semibold mt-24">
-      <p>Lene Marquardsen | Sabine Lauge Olsen</p>
+      <p>Sabine Lauge Olsen | Gry Kruuse</p>
       <p>
         Hovedgaden 28 | 4622 Havdrup Tlf. 46 18 55 80
         info@tandlaegehuset-havdrup.dk{' '}
